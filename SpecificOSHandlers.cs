88a97e2e8f8bdf62cc91eab8aa181fa96465bfe9
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2025-04-01 19:15:18 -0700

    Add platform specific handling

--- SpecificOSHandlers.cs
+++ SpecificOSHandlers.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,9 +19,15 @@ internal static partial class OS_Handlers
 
     public static void InitializeConsole(string[] args)
     {
-        //#if Windows
+        //DEBUG - Get os and current target framework
+        //OperatingSystem os = Environment.OSVersion;
+        //string? targetFramework = System.Reflection.Assembly.GetExecutingAssembly().GetCustomAttribute<System.Runtime.Versioning.TargetFrameworkAttribute>()?.FrameworkName;
+
+        #if WINDOWS
             WindowsNativeMethods.AllocConsole();
-        //#endif
+        #else
+            WriteError("Error: Can only use \"background\" mode (without console window) on Windows.");
+        #endif
     }
 }
 
