e444d353396b51aac12e48f304341cc92235681c
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2025-04-11 23:09:04 -0700

    Support attaching to existing console

--- Handlers/SpecificOSHandlers.cs
+++ Handlers/SpecificOSHandlers.cs
@@ -10,9 +10,14 @@ namespace RewindSubtitleDisplayerForPlex;
 
 internal static partial class OS_Handlers
 {
+    private const uint ATTACH_PARENT_PROCESS = 0xFFFFFFFF; // -1
 
     internal partial class WindowsNativeMethods
     {
+        [LibraryImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static partial bool AttachConsole(uint dwProcessId);
+
         [LibraryImport("kernel32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static partial bool AllocConsole();
@@ -27,16 +32,58 @@ internal static partial class OS_Handlers
 
         // Windows
         if (OperatingSystem.IsWindows())
+        {
+            string osMessage = "Windows OS detected.";
+
             // Default is to NOT run in background mode, so allocate console
             if (runInBackgroundArg == false)
             {
-                WindowsNativeMethods.AllocConsole();
-                return false;
+                string bgMode = "Background mode is not enabled. Allocating console for the process.";
+                // Attempt to attach to the parent process's console.
+                bool attached;
+                attached = WindowsNativeMethods.AttachConsole(ATTACH_PARENT_PROCESS);
+
+                // If we are not attached to a console, we need to allocate one.
+                if (!attached)
+                {
+                    string noParentConsole = "No parent console found. Attempting to allocate a new console for the process.";
+                    // Allocate a new console for the process.
+                    bool allocated = WindowsNativeMethods.AllocConsole();
+
+                    // If allocation failed, we can use logging again because it doesn't matter anyway
+                    if (!allocated)
+                    {
+                        LogDebug(osMessage);
+                        LogDebug(bgMode);
+                        LogDebug(noParentConsole);
+                        LogError("Error: Failed to allocate console. Exiting.");
+                    }
+                    else
+                    {
+                        LogDebug(osMessage);
+                        LogDebug(bgMode);
+                        LogDebug(noParentConsole);
+                        LogDebug("Console allocated successfully.");
+                    }
+
+
+                    return !allocated; // if we allocated a console, we are not in background mode.
+                }
+                else
+                {
+                    LogDebug(osMessage);
+                    LogDebug(bgMode);
+                    LogDebug("Attached to parent console successfully.");
+                    return false; // Successfully attached to the parent console, so we are not in background mode.
+                }
+
             }
             else // To run in background mode we do nothing
             {
+                LogDebug("Background mode is enabled. No console will be allocated.");
                 return true;
             }
+        }
         // Not windows
         else
         {
